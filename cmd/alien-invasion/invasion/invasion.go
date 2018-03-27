6f29d94587193f2ff20b51a1210b08a1f308258d
Norbert Csongradi <norbert@csongradi.hu>
2018-03-27 08:03:35 +0200

    Fixed all func comments to Go style (hopefully)

--- cmd/alien-invasion/invasion/invasion.go
+++ cmd/alien-invasion/invasion/invasion.go
@@ -21,6 +21,7 @@ type City struct {
     Alien int
 }
 
+// Invasion holds a full map and implements methods to run simulation
 type Invasion map[string]*City
 
 var iteration int
@@ -31,8 +32,7 @@ func init() {
     iteration = 0
 }
 
-// Function runSimulation: main simulation loop
-// Input: mapType map data, int number of aliens, int iterations to run
+// Runs the main simulation loop: deploys and moves aliens, prints events
 func (mapData *Invasion) Run(numAliens int, iterations int) {
     _debug(fmt.Sprintf("Deploying %d aliens into cities...", numAliens))
     mapData.Deploy(numAliens)
@@ -42,9 +42,7 @@ func (mapData *Invasion) Run(numAliens int, iterations int) {
     }
 }
 
-// Function readMap: reads in data from file, removes line endings
-// Input: fileName to read from
-// Returns: lines, slice of strings
+// Reads in data from map file, removes line endings, returns all lines
 func (mapData *Invasion) ReadMap(fileName string) []string {
     var result []string
 
@@ -74,9 +72,7 @@ func (mapData *Invasion) ReadMap(fileName string) []string {
     return result
 }
 
-// Function buildMap: builds map hash from lines to Cities
-// Input: stringslice lines
-// Returns: map of City (key: name of City, value: struct City)
+// builds map hash from lines into Cities, stores map data in caller struct
 func (mapData *Invasion) BuildMap(fileName string) {
     mapLines := mapData.ReadMap(fileName)
 
@@ -106,9 +102,7 @@ func (mapData *Invasion) BuildMap(fileName string) {
     *mapData = cities
 }
 
-// Function validateRoads: walks all defined roads and validates the source and destination points
-// Input: mapType mapData with full map
-// Returns: void, but raises exception for validation errors
+// walks all defined roads and validates the source and destination points, bails if missing cities found
 func (mapData *Invasion) validateRoads() {
     for _, city := range *mapData {
         for _, direction := range AllRoads(city) {
@@ -125,9 +119,7 @@ func (mapData *Invasion) validateRoads() {
     }
 }
 
-// Function destroyCity: removes a city from mapData, and removes it from neighbour cities as well
-// Input: *mapType mapData, string cityToBeRemoved
-// Returns: -
+// removes a city from map, and removes it from neighbour cities as well, prints destruction fact
 func (mapData *Invasion) DestroyCity(cityName string, alien1 int, alien2 int) {
     city := (*mapData)[cityName]
 
@@ -141,9 +133,7 @@ func (mapData *Invasion) DestroyCity(cityName string, alien1 int, alien2 int) {
     mapData.AssertAnyCitiesLeft()
 }
 
-// Function deployAliens: initially deploys aliens into cities randomly, takes care of 2 aliens in the same city destroys the city
-// Input: *mapType mapData, int numAliens
-// Returns: -
+// initially deploys aliens into cities randomly, takes care of 2 aliens in the same city destroys the city
 func (mapData *Invasion) Deploy(numAliens int) {
     for i := 1; i <= numAliens; i++ {
         allCities := mapData.AllCities() // need to re-read city keys as they could be destroyed during deployment
@@ -155,12 +145,15 @@ func (mapData *Invasion) Deploy(numAliens int) {
     }
 }
 
-// Function
+// iterates over all cities and moves aliens if roads are still present
 func (mapData *Invasion) Move() {
     allCities := mapData.AllCities()
 
     for _, cityName := range allCities {
         if city, ok := (*mapData)[cityName]; ok {
+            if city.Alien == 0 { //no alien here to move
+                continue
+            }
             if roads := AllRoads(city); len(roads) > 0 { // there are still roads out of this city
                 directionIndex := rand.Intn(len(roads))
                 cityTo := city.Roads[roads[directionIndex]]
@@ -171,6 +164,7 @@ func (mapData *Invasion) Move() {
     }
 }
 
+// moves alien into a City, calls destroy if another alien is already there
 func (mapData *Invasion) MoveAlienTo(cityName string, alien int) {
     city := (*mapData)[cityName]
 
@@ -181,6 +175,7 @@ func (mapData *Invasion) MoveAlienTo(cityName string, alien int) {
     }
 }
 
+// terminates the simulation if all of the cities have been destroyed
 func (mapData *Invasion) AssertAnyCitiesLeft() {
     if len(mapData.AllCities()) == 0 {
         log.Printf("[iter %5d] all cities (%d) have been destroyed", iteration, maxCities)
@@ -189,22 +184,19 @@ func (mapData *Invasion) AssertAnyCitiesLeft() {
 
 }
 
-// Function dirNameToInt: convert a direction name (north, ...) to integer value
-// Input: string direction
-// Returns: integer representation of direction
+// convert a direction name (north, ...) to integer value
 func dirNameToInt(direction string) int {
     dirHash := map[string]int{"north": 0, "east": 1, "south": 2, "west": 3}
 
     return dirHash[direction]
 }
 
-// Function oppositeDirection: returns the int representation of oppsite direction
-// Input: direction int (eg. north is 0, etc.)
-// Returns: direction int (eg. 2 for south, etc.)
+// returns the int representation of oppsite direction
 func oppositeDirection(direction int) int {
     return (direction + 2) % 4
 }
 
+// returns all city names of full map
 func (mapData *Invasion) AllCities() []string {
     cities := make([]string, len(*mapData))
 
@@ -217,13 +209,7 @@ func (mapData *Invasion) AllCities() []string {
     return cities
 }
 
-func (mapData *Invasion) Dump() {
-    spew.Dump(mapData)
-}
-
-// Function AllRoads: returns a slice of ints with roads from that city, simplifies other loops
-// Input: City city
-// Returns: []int roads
+// returns a slice of ints with roads from that city, simplifies other loops
 func AllRoads(city *City) []int {
     var roads []int
 
@@ -236,6 +222,12 @@ func AllRoads(city *City) []int {
     return roads
 }
 
+// debug, dumps full map data
+func (mapData *Invasion) Dump() {
+    spew.Dump(mapData)
+}
+
+// internal debug printer, controlled by DEBUG bool
 func _debug(obj ...interface{}) {
     if !DEBUG {
         return
